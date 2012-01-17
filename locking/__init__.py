4b185ced5d0b6049ed1092146e179891996ec1f8
Gert Van Gool <gertvangool@gmail.com>
2012-01-17 06:44:20 +0100

    Updates __version__ str generation
    If the last element of VERSION is an empty string, don't append that
    either.

--- locking/__init__.py
+++ locking/__init__.py
@@ -6,7 +6,7 @@ def get_version():
     '''
     Converts the :attr:`VERSION` into a nice string
     '''
-    if len(VERSION) > 3 and VERSION[3] != 'final':
+    if len(VERSION) > 3 and VERSION[3] not in ('final', ''):
         return '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
     else:
         return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
