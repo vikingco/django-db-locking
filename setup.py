f108f6efc7fdbc716e855a6ed89c0c2706388662
gordon <wgordonw1@gmail.com>
2015-09-06 17:17:47 -0400

    https://github.com/vikingco/django-locking/issues/7

--- setup.py
+++ setup.py
@@ -1,6 +1,32 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as SetuptoolsTestCommand
 import locking
 
+class RunTestsCommand(SetuptoolsTestCommand):
+    def initialize_options(self):
+        SetuptoolsTestCommand.initialize_options(self)
+        self.test_suite = "override"
+    
+    def finalize_options(self):
+        SetuptoolsTestCommand.finalize_options(self)
+        self.test_suite = None
+        
+    def run(self):
+        SetuptoolsTestCommand.run(self)
+        self.with_project_on_sys_path(self.run_tests)
+    
+    def run_tests(self):
+        import os
+        import subprocess
+        import sys
+        
+        env = os.environ.copy()
+        env["PYTHONPATH"] = os.pathsep.join(sys.path)
+        
+        cmd = [sys.executable, 'test_project/manage.py', 'test']
+        errno = subprocess.call(cmd, env=env)
+        
+        raise SystemExit(errno)
 
 setup(
     name="django-locking",
@@ -11,6 +37,8 @@ setup(
     long_description=open('README.rst', 'r').read(),
     author='City Live',
     packages=find_packages('.'),
+    cmdclass={'test': RunTestsCommand},
+    tests_require=["django"],
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
